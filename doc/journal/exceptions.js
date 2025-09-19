5ec16d1af6acacab7bbeb1fb235adf1ab9012a22
ASETML <179593370+ASETML@users.noreply.github.com>
2025-09-19 13:28:23 +0200

    chore(JDT): Ajout du fichier d'exception
    [5min] [Done]

---
+++ doc/journal/exceptions.js
@@ -0,0 +1,11 @@
+const exceptions = [
+  {
+    "patch": true,
+    "name": "absence(projet): Remise des prix",
+    "description": "",
+    "date": "2025-09-12T12:45:00.000Z",
+    "duration": 90,
+    "status": "Done",
+    "author": "ASETML"
+  }
+]
