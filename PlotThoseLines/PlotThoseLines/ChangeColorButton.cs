cccf91dd5909cb756e299f46c787379dc77ea321
ASETML <alban.segalen@eduvaud.ch>
2025-10-31 15:17:30 +0100

    chore(ChangeColorButton): Commentaire
    [10min] [Done]

--- PlotThoseLines/PlotThoseLines/ChangeColorButton.cs
+++ PlotThoseLines/PlotThoseLines/ChangeColorButton.cs
@@ -10,29 +10,40 @@ namespace PlotThoseLines
 {
     partial class ChangeColorButton : Button
     {
-        private Serie _serie;
-        public event EventHandler ColorChanged;
+        private Serie _serie; //La série dont il faut changer la couleur
+        public event EventHandler ColorChanged; //Evenement lorsque la couleur à été changéee
 
         public ChangeColorButton()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Open the color picker and change the serie color
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnClick(EventArgs e)
         {
+            //Color picker
             ColorDialog colorDialog = new ColorDialog();
             colorDialog.FullOpen = true;
             colorDialog.Color = this._serie.Color.ToSDColor();
             colorDialog.ShowDialog();
 
+            //Change la couleur de la série
             Program.series.First(s => s.Id == this._serie.Id).ChangeColor(colorDialog.Color);
             SaveFile.UpdateSerieColor(this._serie);
 
+            //Change la couleur du bouton
             this.BackColor = _serie.Color.ToSDColor();
             this.ForeColor = _serie.Color.ToSDColor();
             ColorChanged.Invoke(this, EventArgs.Empty);
         }
 
+        /// <summary>
+        /// Link a serie to this control
+        /// </summary>
+        /// <param name="s"></param>
         public void BindSerie(Serie s)
         {
             this._serie = s;
